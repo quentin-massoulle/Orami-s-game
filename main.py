d7d5049ce50229106a50ef2201fb46a51eb9d0b5
quentin-massoulle <quentin.massoulle@etud.u-picardie.fr>
2024-09-10 12:29:31 +0200

    creation de la classe game

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
 import pygame 
 pygame.init()
-
-pygame.display.set_mode((1200,800))
-pygame.display.set_caption("orami adventure")
-
-
-runing = True
-
-while runing :
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            runing = False
-pygame.quit()
