69a7ef8b66871f23c29649088e987a60814285b7
quentin-massoulle <quentin.massoulle@etud.u-picardie.fr>
2024-09-16 09:54:21 +0200

    ajoue deplacemetn joueur

--- class_game/game.py
+++ class_game/game.py
@@ -19,25 +19,42 @@ class Game:
         map_layer.zoom = 3
 
         # Gérer le joueur
-        self.player = Player()  # Instancier le joueur
+        player_position = tmx_data.get_object_by_name("player")
+        self.player = Player(player_position.x, player_position.y)  # Instancier le joueur
 
         # Dessiner le groupe de calques
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
         self.group.add(self.player)
+    
+    # Récupérer les touches appuyées et déplacer le joueur
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]  or pressed[pygame.K_z]:
+            self.player.mouve(0,- self.player.vitesse)
+        if pressed[pygame.K_DOWN]  or pressed[pygame.K_s]:
+            self.player.mouve(0,self.player.vitesse)
+        if pressed[pygame.K_LEFT]  or pressed[pygame.K_q]:
+            self.player.mouve(-self.player.vitesse,0)
+        if pressed[pygame.K_RIGHT]  or pressed[pygame.K_d]:
+            self.player.mouve(self.player.vitesse,0)
 
     def run(self):
+        clock = pygame.time.Clock()  # Pour gérer le taux de rafraîchissement
         running = True
+
         while running:
+            # Dessiner la carte et les sprites
+            self.handle_input()
+            self.group.update()
+            self.group.center(self.player.rect.center)  # Centrer la caméra sur le joueur
+            self.group.draw(self.screen)
+            
             # Gérer les événements
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            # Dessiner la carte et les sprites
-            self.group.center(self.player.rect.center)  # Centrer la caméra sur le joueur
-            self.group.update()
-            self.group.draw(self.screen)
-
             pygame.display.flip()  # Rafraîchir l'écran avec flip()
+            clock.tick(60)  # Limiter le jeu à 60 FPS
 
         pygame.quit()
--- class_game/player.py
+++ class_game/player.py
@@ -1,13 +1,24 @@
+from typing import Any
 import pygame
 pygame.init()
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,x,y):
         super().__init__()  # Correction de l'appel à super()
         self.sprite_sheet = pygame.image.load("player_creature_asset/Player.png").convert_alpha()  # Chargement de l'image avec transparence
         self.image = self.get_image(0, 0)  # Récupérer la première image du sprite
         self.rect = self.image.get_rect()  # Récupérer le rectangle de l'image
+        self.position = [x,y]
+        self.vitesse=2
+    def update(self):
+        self.rect.topleft = self.position
 
+    #deplacement du joueur 
+    def mouve(self, x, y):
+        # Déplacement du joueur (modifie sa position)
+        self.position[0] += x
+        self.position[1] += y
+    
     def get_image(self, x, y):
         # Découpe l'image du sprite sheet
         image = pygame.Surface([32, 32], pygame.SRCALPHA)  # Support avec transparence
